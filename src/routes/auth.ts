719cea1f3f88c427b5d19372710e5a1c0be167dd
Priyabrat  Duarah <priyabrat@Priyabrats-MacBook-Air.local>
2024-01-16 23:40:25 +0530

    route for checkAdmin created

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 
 import * as Controllers from "@controllers";
+import * as Middlewares from "@middlewares";
 
 const router: Router = Router({ mergeParams: true });
 
 router.post("/signUp", Controllers.Auth.signUp);
+
+router.get("/isAdmin", Middlewares.Auth.isAdmin, Controllers.Auth.checkAdmin);
 
 export default router;
