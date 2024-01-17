9263083ac7e4b65dd7a21a3935822a929e742751
Priyabrat  Duarah <priyabrat@Priyabrats-MacBook-Air.local>
2024-01-17 23:52:06 +0530

    fixed merge conflicts

--- src/controllers/event/update.ts
+++ src/controllers/event/update.ts
@@ -33,13 +33,12 @@ const updateEvent: Interfaces.Controller.Async = async (req, res, next) => {
     where: { id: eventId },
   });
   if (!eventOriginal) return next(Errors.Module.eventNotFound);
-  if (moduleId) {
-    if (!moduleId || typeof moduleId !== "string" || eventId.length !== 24)
-      return next(Errors.Module.invalidInput);
-    if (!(await prisma.module.findFirst({ where: { id: moduleId } })))
-      return next(Errors.Module.moduleNotFound);
-  }
   if (minTeamSize > maxTeamSize) return next(Errors.Module.invalidInput);
+  if (!moduleId || typeof moduleId !== "string" || moduleId.length !== 24)
+    return next(Errors.Module.invalidInput);
+  if (!(await prisma.module.findFirst({ where: { id: moduleId } })))
+    return next(Errors.Module.moduleNotFound);
+
   let regStart;
   if (registrationStartTime) regStart = new Date(registrationStartTime);
   else {
@@ -57,19 +56,10 @@ const updateEvent: Interfaces.Controller.Async = async (req, res, next) => {
   if (regStart && regEnd && regStart > regEnd)
     return next(Errors.Module.invalidInput);
 
-  const { organizers, managers }: { organizers: [string]; managers: [string] } =
-    req.body;
-
-  let organizersUsernames;
-  if (organizers) {
-    organizersUsernames = await Utils.Event.extractUsername(organizers);
-    if (!organizersUsernames) return next(Errors.User.userNotFound);
-  }
-  let managersUsernames;
-  if (managers) {
-    managersUsernames = await Utils.Event.extractUsername(managers);
-    if (!managersUsernames) return next(Errors.User.userNotFound);
-  }
+  const {
+    organizers = [],
+    managers = [],
+  }: { organizers: string[]; managers: string[] } = req.body;
 
   if (
     (registrationIncentive && !(typeof registrationIncentive === "number")) ||
@@ -108,6 +98,107 @@ const updateEvent: Interfaces.Controller.Async = async (req, res, next) => {
   )
     return next(Errors.Module.invalidInput);
 
+  const userExist = await Promise.all([
+    ...organizers.map(async (id: string) => {
+      const user = await prisma.user.findFirst({ where: { id: id } });
+
+      if (!user) {
+        return false;
+      } else {
+        return true;
+      }
+    }),
+    ...managers.map(async (id: string) => {
+      const user = await prisma.user.findFirst({ where: { id: id } });
+
+      if (!user) {
+        return false;
+      } else {
+        return true;
+      }
+    }),
+  ]);
+
+  if (!userExist.every((result) => result)) {
+    return next(Errors.User.userNotFound);
+  }
+
+  const eventOrganisers = await prisma.eventOrganiser.findMany({
+    where: {
+      eventId: eventId,
+    },
+  });
+
+  const eventOrganiserIds = eventOrganisers.map(
+    (organiser) => organiser.userId
+  );
+
+  const organiserIdsToRemove = eventOrganiserIds.filter(
+    (id) => !organizers.includes(id)
+  );
+
+  const eventManagers = await prisma.eventManager.findMany({
+    where: {
+      eventId: eventId,
+    },
+  });
+
+  const eventManagersUserId = eventManagers.map((manager) => manager.userId);
+
+  const managerIdsToRemove = eventManagersUserId.filter(
+    (id) => !managers.includes(id)
+  );
+
+  await Promise.all([
+    eventOrganisers,
+    eventManagers,
+    ...organiserIdsToRemove.map(async (id: string) => {
+      await prisma.eventOrganiser.delete({
+        where: {
+          userId_eventId: { userId: id, eventId: eventId },
+        },
+      });
+    }),
+    ...managerIdsToRemove.map(async (id: string) => {
+      await prisma.eventManager.delete({
+        where: {
+          userId_eventId: {
+            userId: id,
+            eventId: eventId,
+          },
+        },
+      });
+    }),
+    ...organizers.map(async (id: string) => {
+      console.log(id);
+      await prisma.eventOrganiser.upsert({
+        create: {
+          user: { connect: { id: id } },
+          event: { connect: { id: eventId } },
+        },
+        update: {},
+        where: {
+          userId_eventId: { userId: id, eventId: eventId },
+        },
+      });
+    }),
+    ...managers.map(async (id: string) => {
+      await prisma.eventManager.upsert({
+        create: {
+          user: { connect: { id: id } },
+          event: { connect: { id: eventId } },
+        },
+        update: {},
+        where: {
+          userId_eventId: {
+            userId: id,
+            eventId: eventId,
+          },
+        },
+      });
+    }),
+  ]);
+
   const event = await prisma.event.update({
     where: { id: eventId },
     data: {
@@ -126,12 +217,6 @@ const updateEvent: Interfaces.Controller.Async = async (req, res, next) => {
       stagesDescription,
       venue,
       moduleId,
-      organizers: {
-        connect: organizersUsernames,
-      },
-      managers: {
-        connect: managersUsernames,
-      },
       extraQuestions: extraQuestions,
     },
   });
