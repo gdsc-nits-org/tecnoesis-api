2f3ea7ff5526f566975bc017b34c3b041c990723
Sharthak Sharma <sharthaksharma.ss@gmail.com>
2024-09-02 20:17:52 +0530

    change add organizer method to accept username instead of id

--- src/controllers/event/addOrganizer.ts
+++ src/controllers/event/addOrganizer.ts
@@ -28,35 +28,37 @@ const addOrganizer: Interfaces.Controller.Async = async (req, res, next) => {
     (value, index, array) => array.indexOf(value) === index
   );
 
-  if (!organizers.every((organizer) => organizer.length === 24)) {
-    return next(Errors.Module.invalidInput);
-  }
-
-  const userIdExist = await Utils.Event.userIdExist(organizers);
-
-  if (!userIdExist) {
-    return next(Errors.User.userNotFound);
-  } else {
-    const eventOrganisers: EventOrganiser[] = [];
-    await Promise.all(
-      organizers.map(async (id: string) => {
-        const eventOrganizer = await prisma.eventOrganiser.upsert({
-          create: {
-            user: { connect: { id: id } },
-            event: { connect: { id: eventId } },
-          },
-          where: { userId_eventId: { userId: id, eventId: eventId } },
-          update: {},
-        });
-
-        if (eventOrganizer) {
-          eventOrganisers.push(eventOrganizer);
-        }
+  const results = await Promise.all(
+    organizers.map((username: string) =>
+      prisma.user.findFirst({
+        where: { username: username },
       })
-    );
+    )
+  );
 
-    return res.json(Utils.Response.Success(eventOrganisers));
+  if (!results.every((result) => result)) {
+    return next(Errors.User.userNotFound);
   }
+
+  const eventOrganisers: EventOrganiser[] = [];
+  await Promise.all(
+    results.map(async (result) => {
+      const eventOrganizer = await prisma.eventOrganiser.upsert({
+        create: {
+          user: { connect: { id: result!.id } },
+          event: { connect: { id: eventId } },
+        },
+        where: { userId_eventId: { userId: result!.id, eventId: eventId } },
+        update: {},
+      });
+
+      if (eventOrganizer) {
+        eventOrganisers.push(eventOrganizer);
+      }
+    })
+  );
+
+  return res.json(Utils.Response.Success(eventOrganisers));
 };
 
 export { addOrganizer };
