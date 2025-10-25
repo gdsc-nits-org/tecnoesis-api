3ac52eae73a52fd7b999368b112f540e0b576985
rishinhk004 <rishirajsaha555@gmail.com>
2025-10-25 18:53:27 +0530

    fix

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -1,8 +1,6 @@
 import multer from "multer";
 import multerS3 from "multer-s3";
-
 import { initializeAWS, s3 } from "@utils/aws";
-// import * as Constants from "@constants";
 
 let upload: multer.Multer;
 
@@ -13,13 +11,17 @@ function initializeMulter() {
       s3,
       cacheControl: "max-age=31536000",
       bucket: process.env.AWS_S3_BUCKET_NAME!,
-      acl: "public-read",
+      // acl: "public-read", // <-- THIS LINE IS REMOVED
       contentDisposition: "inline",
       contentType: multerS3.AUTO_CONTENT_TYPE,
       key: function (_, file, cb) {
         cb(null, Date.now().toString() + "_" + file.originalname);
       },
     }),
+    // ADD THIS 'limits' OBJECT TO FIX THE 413 ERROR
+    limits: {
+      fileSize: 50 * 1024 * 1024, // Set to 50MB (or your desired limit)
+    },
   });
 }
 
